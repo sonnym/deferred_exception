6e36e11482eb80adf861464ccd564c7c8abf9b69
Leon Miller-Out <leon@singlebrook.com>
2013-10-14 14:51:37 -0400

    Remove a duplicate test

--- test/lib/deferred_exception/exception_set.rb
+++ test/lib/deferred_exception/exception_set.rb
@@ -6,10 +6,4 @@ describe DeferredException::ExceptionSet do
     exception_set = DeferredException::ExceptionSet.new(exceptions)
     assert_equal "foo\n=====\nbar", exception_set.message
   end
-
-  it 'should return a compound message' do
-    exceptions = [StandardError.new('foo'), StandardError.new('bar')]
-    exception_set = DeferredException::ExceptionSet.new(exceptions)
-    assert_equal "foo\n=====\nbar", exception_set.message
-  end
 end
